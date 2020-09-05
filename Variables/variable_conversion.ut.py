a658a97f019a8c25ac77c92d314a465e7a272a93
Michael Walter <michael@MacBook-Pro.local>
2020-09-04 23:56:42 -0700

    Created 5 aditional tests and 1 additional problem.

--- Variables/variable_conversion.ut.py
+++ Variables/variable_conversion.ut.py
@@ -4,6 +4,13 @@ from variable_conversions import varConversion
 class varTests(unittest.TestCase):
     def test_toInt(self):
         self.assertEqual(varConversion.toInt("5"), 5)
+        self.assertEqual(varConversion.toInt("77"),77)
+        self.assertEqual(varConversion.toInt("9284"),9284)
+
+    def test_toStr(self):
+        self.assertEqual(varConversion.toStr(4),"4")
+        self.assertEqual(varConversion.toStr(993),"993")
+        self.assertEqual(varConversion.toStr(107),"107")
 
 if __name__ == '__main__':
     unittest.main()
